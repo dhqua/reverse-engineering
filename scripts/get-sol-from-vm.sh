6119674a5ca0cbd5e861bd4e65cb5a185ca0d8a9
Qua <quaon.thomas1@gmail.com>
2019-06-05 12:00:18 -0600

    Added usage to vm scripts

--- scripts/get-sol-from-vm.sh
+++ scripts/get-sol-from-vm.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
+echo "Usage: ./get-sol-from-vm file"
+echo "Will be retrieved from the crackmes folder"
 scp qua@192.168.56.104:~/crackmes/$1.sol .
--- scripts/send-to-vm.sh
+++ scripts/send-to-vm.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
+echo "Usage: ./send-to-vm file"
+echo "Will be saved in the crackmes folder by default"
 scp $1 qua@192.168.56.104:~/crackmes
